c3805eae57aff31da184c23142d750ed442cb8c2
Arpit Sinha <mr.arpitsinha@gmail.com>
2026-05-11 21:10:14 +0530

    feat: added sign in and sign up form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Headers from './Header';
+
+
 const Login = () => {
+
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleIsSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  }
   return (
-    <div>
+    <div className="relative min-h-screen">
         <Headers />
-        <div className="h-screen w-full">
+        <div className="absolute inset-0">
           <img
             src="https://assets.nflxext.com/ffe/siteui/vlv3/5bd3572a-0d1b-4228-aaa7-5b2dc45952b2/web/IN-en-20260413-TRIFECTA-perspective_4100808f-7dc6-4c78-8677-18db2989f7bc_large.jpg"
             srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/5bd3572a-0d1b-4228-aaa7-5b2dc45952b2/web/IN-en-20260413-TRIFECTA-perspective_4100808f-7dc6-4c78-8677-18db2989f7bc_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/5bd3572a-0d1b-4228-aaa7-5b2dc45952b2/web/IN-en-20260413-TRIFECTA-perspective_4100808f-7dc6-4c78-8677-18db2989f7bc_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/5bd3572a-0d1b-4228-aaa7-5b2dc45952b2/web/IN-en-20260413-TRIFECTA-perspective_4100808f-7dc6-4c78-8677-18db2989f7bc_small.jpg 959w"
             alt=""
             aria-hidden="true"
-            className="w-full h-screen object-cover"
+            className="w-full h-full object-cover"
           />
+          <div className="absolute inset-0 bg-black/50" />
         </div>
-        <form className='absolute top-1/2 left-1/2 bg-black bg-opacity-75 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center text-white w-[300px] md:w-[400px] rounded-md flex-wrap p-4'>
-            <h1 className='font-bold text-3xl py-4'>Sign In</h1>
-            <input type="text" placeholder='Email Address' className='p-2 m-2 bg-gray-700 w-full rounded-md outline-none' />
-            <input type="password" placeholder='Password' className='p-2 m-2 bg-gray-700 w-full rounded-md outline-none' />  
-            <button className='p-4 my-6 bg-red-600 hover:bg-red-700 w-full rounded-md'>Sign In</button>  
-        </form>
+        <div className="relative z-10 flex min-h-screen items-center justify-center px-4">
+          <form className='w-full max-w-md bg-black/80 rounded-md p-8 text-white space-y-6'>
+            <h1 className='font-bold text-3xl text-center'>{isSignInForm ? 'Sign In' : 'Sign Up'}</h1>
+            <div className='space-y-4'>
+              {!isSignInForm && (<input type="text" placeholder='Full Name' className='w-full rounded-md border border-white/10 bg-gray-800 p-3 text-sm outline-none' />)}
+              <input type="text" placeholder='Email Address' className='w-full rounded-md border border-white/10 bg-gray-800 p-3 text-sm outline-none' />
+              <input type="password" placeholder='Password' className='w-full rounded-md border border-white/10 bg-gray-800 p-3 text-sm outline-none' />
+              <button className='w-full rounded-md bg-red-600 px-4 py-3 text-base font-semibold hover:bg-red-700'>{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
+            </div>
+            <p className='text-center text-sm text-white/80 cursor-pointer' onClick={toggleIsSignInForm}>
+              {isSignInForm ? "Don't have an account? Sign Up Now" : "Already have an account? Sign In Now"}
+            </p>
+          </form>
+        </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
